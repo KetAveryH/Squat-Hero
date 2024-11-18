6fad38c872d782056854a016230ccd79d18d34d9
Ket Hollingsworth <khollingsworth@g.hmc.edu>
2024-11-18 15:45:47 -0800

    VGA code Compiles

---
+++ FPGA/squat_hero/test_1/squat_hero_test_1_synthesize.tcl
@@ -0,0 +1,19 @@
+if {[catch {
+
+# define run engine funtion
+source [file join {C:/lscc/radiant/2024.1} scripts tcl flow run_engine.tcl]
+# define global variables
+global para
+set para(gui_mode) "1"
+set para(prj_dir) "C:/Users/KetHollingsworth/Desktop/Repositories/Microps/Squat-Hero/squat_hero"
+# synthesize IPs
+# synthesize VMs
+# synthesize top design
+file delete -force -- squat_hero_test_1.vm squat_hero_test_1.ldc
+::radiant::runengine::run_engine_newmsg synthesis -f "squat_hero_test_1_lattice.synproj" -logfile "squat_hero_test_1_lattice.srp"
+::radiant::runengine::run_postsyn [list -a iCE40UP -p iCE40UP5K -t SG48 -sp High-Performance_1.2V -oc Industrial -top -w -o squat_hero_test_1_syn.udb squat_hero_test_1.vm] [list C:/Users/KetHollingsworth/Desktop/Repositories/Microps/Squat-Hero/squat_hero/test_1/squat_hero_test_1.ldc]
+
+} out]} {
+   ::radiant::runengine::runtime_log $out
+   exit 1
+}
