a3ea441c8f8ce21c633b26093dcfce896243aa1b
jasonbowman55 <95401132+jasonbowman55@users.noreply.github.com>
2024-12-04 14:52:12 -0800

    bad code

--- MCU/segger_proj/Output/Debug/Obj/squat_hero_v1/stm32l432xx_Vectors.d
+++ MCU/segger_proj/Output/Debug/Obj/squat_hero_v1/stm32l432xx_Vectors.d
@@ -1,2 +1,2 @@
 Output/Debug/Obj/squat_hero_v1/stm32l432xx_Vectors.o: \
- /Users/jasonbowman/Desktop/Micro-Ps/Squat-Hero/MCU/segger_proj/STM32L4xx/Source/stm32l432xx_Vectors.s
+ C:\Users\spenc\OneDrive\Documents\Desktop\Squat-Hero\MCU\segger_proj\STM32L4xx\Source\stm32l432xx_Vectors.s
