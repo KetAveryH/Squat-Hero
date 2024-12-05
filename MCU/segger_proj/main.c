a1e9c83da03a872c91491c6c6b016a69242aa1e7
jasonbowman55 <95401132+jasonbowman55@users.noreply.github.com>
2024-12-04 16:45:47 -0800

    SPI SENDING CORRECTLY AGAIN

--- MCU/segger_proj/main.c
+++ MCU/segger_proj/main.c
@@ -42,7 +42,7 @@ int main(void) {
     configureClock();   // Configure the system clock
     init_I2C1();        // Initialize the I2C1 peripheral & corresponding GPIOs
     init_I2C3();        // Initialize the I2C3 peripheral & corresponding GPIOs
-    initSPI(1, 0, 0);
+    initSPI(6, 0, 0);
 
     // Step 2: Initialize all of the LSM6DSO32 IMU
     IMU_config_I2C3(IMU_ADDRESS_FEMAR, CTRL1_XL);
