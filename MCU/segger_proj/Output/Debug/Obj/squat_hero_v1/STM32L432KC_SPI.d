bef9bb6b7d06223fff637143db32b64274b144f0
jasonbowman55 <95401132+jasonbowman55@users.noreply.github.com>
2024-12-09 09:45:41 -0800

    session ran

--- MCU/segger_proj/Output/Debug/Obj/squat_hero_v1/STM32L432KC_SPI.d
+++ MCU/segger_proj/Output/Debug/Obj/squat_hero_v1/STM32L432KC_SPI.d
@@ -1,23 +1,23 @@
 Output/Debug/Obj/squat_hero_v1/STM32L432KC_SPI.o: \
-  /Users/jasonbowman/Desktop/Micro-Ps/Squat-Hero/MCU/lib/STM32L432KC_SPI.c \
-  /Users/jasonbowman/Desktop/Micro-Ps/Squat-Hero/MCU/lib/STM32L432KC.h \
-  /Applications/SEGGER/SEGGER\ Embedded\ Studio\ 8.16a/include/stdio.h \
-  /Applications/SEGGER/SEGGER\ Embedded\ Studio\ 8.16a/include/__SEGGER_RTL.h \
-  /Applications/SEGGER/SEGGER\ Embedded\ Studio\ 8.16a/include/__SEGGER_RTL_ConfDefaults.h \
-  /Applications/SEGGER/SEGGER\ Embedded\ Studio\ 8.16a/include/__SEGGER_RTL_Conf.h \
-  /Applications/SEGGER/SEGGER\ Embedded\ Studio\ 8.16a/include/__SEGGER_RTL_Arm_Conf.h \
-  /Applications/SEGGER/SEGGER\ Embedded\ Studio\ 8.16a/include/stdlib.h \
-  /Applications/SEGGER/SEGGER\ Embedded\ Studio\ 8.16a/include/stdint.h \
-  /Users/jasonbowman/Desktop/Micro-Ps/Squat-Hero/MCU/segger_proj/STM32L4xx/Device/Include/stm32l432xx.h \
-  /Users/jasonbowman/Desktop/Micro-Ps/Squat-Hero/MCU/segger_proj/CMSIS_5/CMSIS/Core/Include/core_cm4.h \
-  /Users/jasonbowman/Desktop/Micro-Ps/Squat-Hero/MCU/segger_proj/CMSIS_5/CMSIS/Core/Include/cmsis_version.h \
-  /Users/jasonbowman/Desktop/Micro-Ps/Squat-Hero/MCU/segger_proj/CMSIS_5/CMSIS/Core/Include/cmsis_compiler.h \
-  /Users/jasonbowman/Desktop/Micro-Ps/Squat-Hero/MCU/segger_proj/CMSIS_5/CMSIS/Core/Include/cmsis_gcc.h \
-  /Users/jasonbowman/Desktop/Micro-Ps/Squat-Hero/MCU/segger_proj/CMSIS_5/CMSIS/Core/Include/mpu_armv7.h \
-  /Users/jasonbowman/Desktop/Micro-Ps/Squat-Hero/MCU/segger_proj/STM32L4xx/Device/Include/system_stm32l4xx.h \
-  /Users/jasonbowman/Desktop/Micro-Ps/Squat-Hero/MCU/lib/STM32L432KC_GPIO.h \
-  /Users/jasonbowman/Desktop/Micro-Ps/Squat-Hero/MCU/lib/STM32L432KC_RCC.h \
-  /Users/jasonbowman/Desktop/Micro-Ps/Squat-Hero/MCU/lib/STM32L432KC_TIM.h \
-  /Users/jasonbowman/Desktop/Micro-Ps/Squat-Hero/MCU/lib/STM32L432KC_FLASH.h \
-  /Users/jasonbowman/Desktop/Micro-Ps/Squat-Hero/MCU/lib/STM32L432KC_USART.h \
-  /Users/jasonbowman/Desktop/Micro-Ps/Squat-Hero/MCU/lib/STM32L432KC_SPI.h
+  C:\Users\spenc\OneDrive\Documents\Desktop\HMC\microPs\Squat-Hero\MCU\lib\STM32L432KC_SPI.c \
+  C:\Users\spenc\OneDrive\Documents\Desktop\HMC\microPs\Squat-Hero\MCU\lib\STM32L432KC.h \
+  C:\Program\ Files\SEGGER\SEGGER\ Embedded\ Studio\ for\ ARM\ 7.32a\include\stdio.h \
+  C:\Program\ Files\SEGGER\SEGGER\ Embedded\ Studio\ for\ ARM\ 7.32a\include\__SEGGER_RTL.h \
+  C:\Program\ Files\SEGGER\SEGGER\ Embedded\ Studio\ for\ ARM\ 7.32a\include\__SEGGER_RTL_ConfDefaults.h \
+  C:\Program\ Files\SEGGER\SEGGER\ Embedded\ Studio\ for\ ARM\ 7.32a\include\__SEGGER_RTL_Conf.h \
+  C:\Program\ Files\SEGGER\SEGGER\ Embedded\ Studio\ for\ ARM\ 7.32a\include\__SEGGER_RTL_Arm_Conf.h \
+  C:\Program\ Files\SEGGER\SEGGER\ Embedded\ Studio\ for\ ARM\ 7.32a\include\stdlib.h \
+  C:\Program\ Files\SEGGER\SEGGER\ Embedded\ Studio\ for\ ARM\ 7.32a\include\stdint.h \
+  C:\Users\spenc\OneDrive\Documents\Desktop\HMC\microPs\Squat-Hero\MCU\segger_proj\STM32L4xx\Device\Include\stm32l432xx.h \
+  C:\Users\spenc\OneDrive\Documents\Desktop\HMC\microPs\Squat-Hero\MCU\segger_proj\CMSIS_5\CMSIS\Core\Include\core_cm4.h \
+  C:\Users\spenc\OneDrive\Documents\Desktop\HMC\microPs\Squat-Hero\MCU\segger_proj\CMSIS_5\CMSIS\Core\Include\cmsis_version.h \
+  C:\Users\spenc\OneDrive\Documents\Desktop\HMC\microPs\Squat-Hero\MCU\segger_proj\CMSIS_5\CMSIS\Core\Include\cmsis_compiler.h \
+  C:\Users\spenc\OneDrive\Documents\Desktop\HMC\microPs\Squat-Hero\MCU\segger_proj\CMSIS_5\CMSIS\Core\Include\cmsis_gcc.h \
+  C:\Users\spenc\OneDrive\Documents\Desktop\HMC\microPs\Squat-Hero\MCU\segger_proj\CMSIS_5\CMSIS\Core\Include\mpu_armv7.h \
+  C:\Users\spenc\OneDrive\Documents\Desktop\HMC\microPs\Squat-Hero\MCU\segger_proj\STM32L4xx\Device\Include\system_stm32l4xx.h \
+  C:\Users\spenc\OneDrive\Documents\Desktop\HMC\microPs\Squat-Hero\MCU\lib\STM32L432KC_GPIO.h \
+  C:\Users\spenc\OneDrive\Documents\Desktop\HMC\microPs\Squat-Hero\MCU\lib\STM32L432KC_RCC.h \
+  C:\Users\spenc\OneDrive\Documents\Desktop\HMC\microPs\Squat-Hero\MCU\lib\STM32L432KC_TIM.h \
+  C:\Users\spenc\OneDrive\Documents\Desktop\HMC\microPs\Squat-Hero\MCU\lib\STM32L432KC_FLASH.h \
+  C:\Users\spenc\OneDrive\Documents\Desktop\HMC\microPs\Squat-Hero\MCU\lib\STM32L432KC_USART.h \
+  C:\Users\spenc\OneDrive\Documents\Desktop\HMC\microPs\Squat-Hero\MCU\lib\STM32L432KC_SPI.h
